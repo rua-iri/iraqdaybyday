1cc33f850c1a1c37030b09426805b91349a3c06f
rua-iri <rory.mcguigan@protonmail.com>
2023-02-25 01:17:52 +0000

    add message if no results are returned and improve styling

--- iraqdaybyday/src/components/Main.js
+++ iraqdaybyday/src/components/Main.js
@@ -28,7 +28,7 @@ export default function Main() {
     }
 
     function getToday() {
-        axios.get("https://8f8ach1f5c.execute-api.us-east-1.amazonaws.com/IraqCasualties?type=test")
+        axios.get("https://8f8ach1f5c.execute-api.us-east-1.amazonaws.com/IraqCasualties?type=today")
             .then(result => {
                 setTodayCasualties(result.data);
             })
@@ -36,12 +36,7 @@ export default function Main() {
 
     let blah;
 
-    let casualtiesExist = todayCasualties && todayCasualties.length>0;
-
-    // TODO make this if statement work if no results are returned
-    // for SOME REASON javascript evaluates []!==[] to true!!!!!
-    // therefore if todayCasualties is empty then this still evalutes to true
-    // also an empty array evaluates to true and we can't map an empty array obviously
+    let casualtiesExist = todayCasualties && todayCasualties.length > 0;
 
     if (casualtiesExist) {
         blah = todayCasualties.map((content, key) => (
@@ -49,7 +44,7 @@ export default function Main() {
         ))
     } else {
         // TODO div to appear if no incidents on this day
-        blah = <div>{"blah"}</div>
+        blah = <div>No incidents today</div>
     }
 
     const incidentHeader = <Incident loc="Location" tar="Target" wea="Weapons" minCas="Min Casualties" maxCas="Max Casualties" isTitle={true} />
@@ -58,17 +53,19 @@ export default function Main() {
 
     return (
         <div>
-            <h2 className="total-count">
-                Total Killed So Far:
-            </h2>
-            <div>
-                Minimum: {minTotalKilled}
-            </div>
-            <div>
-                Maximum: {maxTotalKilled}
+            <div className="total-container">
+                <h2 className="total-count">
+                    Total Killed So Far
+                </h2>
+                <div>
+                    Minimum: {minTotalKilled}
+                </div>
+                <div>
+                    Maximum: {maxTotalKilled}
+                </div>
             </div>
 
-            <h2>
+            <h2 className="incidents-today">
                 Incidents Today (<TodayDate />)
             </h2>
 
--- iraqdaybyday/src/components/TodayDate.js
+++ iraqdaybyday/src/components/TodayDate.js
@@ -4,7 +4,7 @@ export default function TodayDate() {
 
     // display today's date in 2003
     const todayDate = new Date();
-    const todayString = `${String(todayDate.getDate()).padStart(2, '0')}/${String(todayDate.getMonth()).padStart(2, '0')}/${todayDate.getFullYear()}`;
+    const todayString = `${String(todayDate.getDate()).padStart(2, '0')}/${String(todayDate.getMonth() + 1).padStart(2, '0')}/${todayDate.getFullYear()}`;
 
 
     return (
